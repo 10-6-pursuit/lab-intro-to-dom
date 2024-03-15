c0cdd2cd4230469da18b6432e717a872d7f206a4
kurtCoding <kjulien7790@gmail.com>
2024-03-15 17:45:41 -0400

    added class

---
+++ main.js
@@ -0,0 +1,6 @@
+const featured = document.querySelector("section").classList.add("featured");
+
+const post = document.createElement("article");
+const element = document.getElementsByClassName("posts");
+
+element.appendChild(post);
