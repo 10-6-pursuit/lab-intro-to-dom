816fd0f6ace224767368d37dfaee6add8be0d81c
lynnjaco <jacobs.lynnette@gmail.com>
2024-03-16 11:18:42 -0400

    adds article to posts section

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const postSection = document.querySelector(".posts");
+postSection.appendChild("article");
+
+const imageSource = "./images/paul-gilmore-unsplash.jpg";
+const imageAltTxt = "Image of a mountain in front of a lake.";
+const heading3Title = "Stop Planning";
+const paraText = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?";
