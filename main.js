48e6795c54031b798a3156fe22376df00a0ffc90
kurtCoding <kjulien7790@gmail.com>
2024-03-16 11:16:24 -0400

    completed tasks

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 const featured = document.querySelector("section").classList.add("featured");
 
 const post = document.createElement("article");
-const element = document.getElementsByClassName("posts");
+post.innerHTML = `<img
+src="./images/paul-gilmore-unsplash.jpg"
+alt="Image of a mountain in front of a lake."
+/>
+<h3>Stop Planning</h3>
+<p>
+You -- yes you! You're an over-planner, I can tell. It's time to stop
+planning so much and instead focusing on relaxing. Taking a break at all is
+so stressful these days; why add to the stress by overworking yourself?
+</p>
+<aside>
+<p>
+  <span><strong>Read Time:</strong> 4 Minutes</span
+  ><a href="#">Read more...</a>
+</p>
+</aside>`;
+const element = document.querySelector(".posts");
 
+const firstArticle = document.querySelector(".posts article");
+firstArticle.remove();
+element.appendChild(firstArticle);
 element.appendChild(post);
+
+const allPosts = document.querySelector("li");
+allPosts.remove();
+
+const readTime = document.querySelector("span");
+readTime.remove();
+
+const lastPost = document.querySelectorAll("article")[2];
+lastPost.remove();
+
+const titles = document.querySelectorAll("h3");
+for (let tite of titles) {
+    tite.remove();
+} 
--- readme.md
+++ readme.md
@@ -44,12 +44,12 @@ To complete this lab, you will need to create and update a number of elements us
 </article>
 ```
 
-- [ ] Move the first article from inside the `section.posts` element to become the second article and move the second article so that it is the first article.
+- [X] Move the first article from inside the `section.posts` element to become the second article and move the second article so that it is the first article.
 
-- [ ] Remove the "All Posts" link from the header.
-- [ ] Remove the `span` element that contains the "Read Time" in the featured post. The featured post is the first post on the page.
-- [ ] Remove the last post from the page, titled "Stop Planning".
-- [ ] Remove all titles from all non-featured posts.
+- [X] Remove the "All Posts" link from the header.
+- [X] Remove the `span` element that contains the "Read Time" in the featured post. The featured post is the first post on the page.
+- [X] Remove the last post from the page, titled "Stop Planning".
+- [X] Remove all titles from all non-featured posts.
 
 ### Reference Final Appearance
 
